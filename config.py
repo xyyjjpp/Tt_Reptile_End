8a3b5efad6bb77d3e2eef70a2c2a0c1aea79de0b
xyyjjpp <61468384@qq.com>
2023-12-27 21:27:50 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import random
+# config
+
+date_outpath = 'C:/Users/Administrator/Desktop/post/化肥/date/'
+key_path = 'keyword.txt'
+qid_path = 'qid.txt'
+cookie_path = 'cookie.txt'
+proxyAddr = "tunnel5.qg.net:16489"
+authKey = "3DB954A8"
+password = "1D842E2A29DB"
+page_number = 10
+fetch_interval = random.randint(5, 10)
---
+++ pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.6 (trial), 000000, 2023-12-27T10:46:31.774546
+from .pyarmor_runtime import __pyarmor__
